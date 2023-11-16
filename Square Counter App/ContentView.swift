1ba682bf2cd32aa159c9ca1024586ba09eae2f53
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-11-16 09:49:12 -0500

    Initial Commit

---
+++ Square Counter App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Square Counter App
+//
+//  Created by Nikita Zhdanov on 2023-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Square Counter App/Square_Counter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Square_Counter_AppApp.swift
+//  Square Counter App
+//
+//  Created by Nikita Zhdanov on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct Square_Counter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
